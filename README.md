c7db1df61a0467c738599ce31a3d2ef5bc9be067
Justin Pierce <brkfstmnchr@gmail.com>
2025-04-30 18:28:33 +0700

    initial commit
    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# React Example
+Simple demonstration using React.js in a modern ASP.NET with Aspire orchestration.
+
+## Build Instructions
+1. Install node.js from https://nodejs.org/en/download/
+2. Build all
---
+++ ReactExample.AppHost/Program.cs
@@ -0,0 +1,14 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var weatherApi = builder.AddProject<Projects.ReactExample_MinimalApi>("weatherapi")
+    .WithExternalHttpEndpoints();
+
+builder.AddNpmApp("react", "../ReactExample.React")
+    .WithReference(weatherApi)
+    .WaitFor(weatherApi)
+    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
+builder.Build().Run();
